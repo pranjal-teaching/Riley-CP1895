67e02e6708fcf8bc77333b07a5b247b16898a72f
Riley Burke <rileyburke0@hotmail.com>
2022-01-26 14:37:51 -0330

    Added files created during class.

---
+++ Lightbulb.py
@@ -0,0 +1,30 @@
+class Lightbulb:
+    def __init__(self, wattage, is_led, brand_name, brightness, is_on=False):
+        self.wattage = wattage
+        self.is_led = is_led
+        self.brand_name = brand_name
+        self.brightness = brightness
+        self.is_on = is_on
+
+    def turn_on(self):
+        self.is_on = True
+
+    def turn_off(self):
+        self.is_on = False
+
+    def to_string(self):
+        print(f"Wattage: {self.wattage}W\nBrand name: {self.brand_name}")
+        if self.is_led:
+            print("LED")
+
+    def set_brightness(self, level):
+        self.brightness = level
+
+
+def main():
+    lightbulb = Lightbulb(80, True, "Phillips", 500)
+    lightbulb.to_string()
+
+
+if __name__ == "__main__":
+    main()
