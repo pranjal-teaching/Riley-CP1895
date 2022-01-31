fdc7aed70b291bdf5d99861ca0daca22f94ce81c
Riley Burke <rileyburke0@hotmail.com>
2022-01-31 08:49:25 -0330

    Created objects.

--- objects.py
+++ objects.py
@@ -5,9 +5,19 @@ class Product:
         self.discount_percent = discount_percent
 
     def get_discount_amount(self):
-        discount_amount = self.price * self.discount_percent / 100
+        discount_amount = round(self.price * self.discount_percent / 100, 2)
         return discount_amount
 
     def get_discount_price(self):
-        discount_price = self.price - self.get_discount_amount()
+        discount_price = round(self.price - self.get_discount_amount(), 2)
         return discount_price
+
+
+def main():
+    product1 = Product('Stanley 13 Ounce Wood Hammer', 12.99, 62)
+    product2 = Product('National Hardware 3/4" Wire Nails', 5.06, 0)
+    print(product1.get_discount_price())
+
+
+if __name__ == "__main__":
+    main()
